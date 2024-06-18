eff2b7cfe46d77472ce9db963b35912debbe6eb4
Dhayal1234 <ankitdhayal00004@gmail.com>
2024-06-18 16:59:17 +0530

    ME_BUILDOUT_XMEME_JAVA_MODULE_BASIC  (Crio.Do)

--- assessment/main.py
+++ assessment/main.py
@@ -111,49 +111,84 @@ class XMemeAssessment(TestCase):
         self.assertEqual(response_length, 0)
 
     # First Post
+    # @pytest.mark.run(order=2)
+    # def test_1_first_post_test(self):
+    #     """Post first MEME and verify that it returns id in the response"""
+    #     endpoint = 'memes/'
+    #     body = {
+    #         'name': 'crio-user',
+    #         'caption': 'crio-meme',
+    #         'url': self.SAMPLE_URL + self.FIRST_POST
+    #     }
+    #     response = self.post_api(endpoint, json.dumps(body))
+    #     # print("verify that response status code is one of " + str(self.POSITIVE_STATUS_CODES))
+    #     self.assertIn(response.status_code, self.POSITIVE_STATUS_CODES)
+    #     data = self.decode_and_load_json(response)
+
+    #     # print('First post data: ', data)
+    #     self.FIRST_POST_ID = data['id']
+    #     # print('Assigned successfully' + str(self.FIRST_POST_ID))
+
     @pytest.mark.run(order=2)
     def test_1_first_post_test(self):
-        """Post first MEME and verify that it returns id in the response"""
         endpoint = 'memes/'
-        body = {
+        body = json.dumps({
             'name': 'crio-user',
             'caption': 'crio-meme',
-            'url': self.SAMPLE_URL + self.FIRST_POST
-        }
-        response = self.post_api(endpoint, json.dumps(body))
-        # print("verify that response status code is one of " + str(self.POSITIVE_STATUS_CODES))
+            'url': self.SAMPLE_URL + '130.png'
+        })
+        response = self.post_api(endpoint, body)
         self.assertIn(response.status_code, self.POSITIVE_STATUS_CODES)
         data = self.decode_and_load_json(response)
-
-        # print('First post data: ', data)
+        self.assertIsNotNone(data.get('id'))
         self.FIRST_POST_ID = data['id']
-        # print('Assigned successfully' + str(self.FIRST_POST_ID))
+
+    # @pytest.mark.run(order=3)
+    # def test_2_get_single_meme(self):  # Score 6
+    #     """Post a new MEME, capture its Id, and verify its GET /meme/{id} returns correct MEME"""
+    #     endpoint = 'memes/'
+    #     body = {
+    #         'name': 'crio-user' + "9999",
+    #         'caption': 'crio-meme' + "9999",
+    #         'url': self.SAMPLE_URL + self.FIRST_POST
+    #     }
+    #     response = self.post_api(endpoint, json.dumps(body))
+    #     # print("verify that response status code is one of " + str(self.POSITIVE_STATUS_CODES))
+    #     self.assertIn(response.status_code, self.POSITIVE_STATUS_CODES)
+    #     data = self.decode_and_load_json(response)
+    #     # print('First post data: ', data)
+
+
+    #     # inserted, now get it using get api.
+    #     endpoint = 'memes/{}'.format(data["id"])
+    #     response = self.get_api(endpoint)
+    #     self.assertIn(response.status_code, self.POSITIVE_STATUS_CODES)
+    #     data = self.decode_and_load_json(response)
+    #     # print('get single: ', data)
+    #     self.assertEqual(data['name'], 'crio-user' + "9999")
+    #     self.assertEqual(data['caption'], 'crio-meme' + "9999")
+    #     self.assertEqual(data['url'], self.SAMPLE_URL + self.FIRST_POST)
+
 
     @pytest.mark.run(order=3)
-    def test_2_get_single_meme(self):  # Score 6
-        """Post a new MEME, capture its Id, and verify its GET /meme/{id} returns correct MEME"""
+    def test_2_get_single_meme(self):
         endpoint = 'memes/'
-        body = {
-            'name': 'crio-user' + "9999",
-            'caption': 'crio-meme' + "9999",
-            'url': self.SAMPLE_URL + self.FIRST_POST
-        }
-        response = self.post_api(endpoint, json.dumps(body))
-        # print("verify that response status code is one of " + str(self.POSITIVE_STATUS_CODES))
+        body = json.dumps({
+            'name': 'crio-user9999',
+            'caption': 'crio-meme9999',
+            'url': self.SAMPLE_URL + '130.png'
+        })
+        response = self.post_api(endpoint, body)
         self.assertIn(response.status_code, self.POSITIVE_STATUS_CODES)
         data = self.decode_and_load_json(response)
-        # print('First post data: ', data)
 
-
-        # inserted, now get it using get api.
-        endpoint = 'memes/{}'.format(data["id"])
+        endpoint = f'memes/{data["id"]}'
         response = self.get_api(endpoint)
         self.assertIn(response.status_code, self.POSITIVE_STATUS_CODES)
         data = self.decode_and_load_json(response)
-        # print('get single: ', data)
-        self.assertEqual(data['name'], 'crio-user' + "9999")
-        self.assertEqual(data['caption'], 'crio-meme' + "9999")
-        self.assertEqual(data['url'], self.SAMPLE_URL + self.FIRST_POST)
+        self.assertEqual(data['name'], 'crio-user9999')
+        self.assertEqual(data['caption'], 'crio-meme9999')
+        self.assertEqual(data['url'], self.SAMPLE_URL + '130.png')
 
 
     @pytest.mark.run(order=4)
